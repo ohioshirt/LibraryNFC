4a32b1156c75204b09d3791b9e55c7cbd3fce3f3
ohioshirt <g.ohioshirt+github@gmail.com>
2024-06-18 01:44:00 +0900

    Fix: missing permission check

--- LibraryHCE/src/main/java/com/piotrekwitkowski/libraryhce/NotificationService.java
+++ LibraryHCE/src/main/java/com/piotrekwitkowski/libraryhce/NotificationService.java
@@ -3,8 +3,9 @@ package com.piotrekwitkowski.libraryhce;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
-import android.os.Build;
+import android.content.pm.PackageManager;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -18,27 +19,31 @@ class NotificationService {
     }
 
     void show(String text) {
-        int randomNotificationId = (int) (Math.random()*1000);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_NAME)
-                .setSmallIcon(R.drawable.ic_stat_name)
-                .setContentTitle(context.getString(R.string.app_name))
-                .setContentText(text)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        int randomNotificationId = (int) (Math.random() * 1000);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_NAME).setSmallIcon(R.drawable.ic_stat_name).setContentTitle(context.getString(R.string.app_name)).setContentText(text).setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         NotificationManagerCompat.from(context).notify(randomNotificationId, builder.build());
     }
 
     void createNotificationChannel(Context ctx) {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String name = ctx.getString(R.string.app_name);
-            NotificationChannel nc = new NotificationChannel(NOTIFICATION_CHANNEL_NAME, name, NotificationManager.IMPORTANCE_DEFAULT);
-            nc.setDescription(NOTIFICATION_CHANNEL_DESCRIPTION);
+        String name = ctx.getString(R.string.app_name);
+        NotificationChannel nc = new NotificationChannel(NOTIFICATION_CHANNEL_NAME, name, NotificationManager.IMPORTANCE_DEFAULT);
+        nc.setDescription(NOTIFICATION_CHANNEL_DESCRIPTION);
 
-            NotificationManager nm = ctx.getSystemService(NotificationManager.class);
-            if (nm != null) {
-                nm.createNotificationChannel(nc);
-            }
+        NotificationManager nm = ctx.getSystemService(NotificationManager.class);
+        if (nm != null) {
+            nm.createNotificationChannel(nc);
         }
     }
 }
